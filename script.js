d4da5e8aec27e88f1d71310b6275896378de7295
acewvrs <song.junyoung64@gmail.com>
2022-06-05 19:54:09 -0500

    Redo evaluate function and use it instead

--- script.js
+++ script.js
@@ -15,24 +15,25 @@ function multiplyNum(a, b) {
 
 function divideNum(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') return 'Not a Number!';
-    if (b === 0) return "can't divide by 0!";
+    if (b === 0) return "ERROR";
     return a / b;
 }
 
-function operator(num1, num2, op) {
+function operate(num1, num2, op) {
+    num1 = Number(num1);
+    num2 = Number(num2);
     if (op === '+') return addNum(num1, num2);
-    else if (op === '-') return subNum(num1, num2);
-    else if (op === '*') return multiplyNum(num1, num2);
-    else if (op === '%') return divideNum(num1, num2);
-    else return "Not a valid operator!";
+    else if (op === '-') return subtractNum(num1, num2);
+    else if (op === 'x') return multiplyNum(num1, num2);
+    else if (op === '/') return divideNum(num1, num2);
+    else return "ERROR";
 }
 
-function evaluate(exp) {
+function evaluate_alt_ver(exp) {
     if (typeof exp !== 'string') return "Invalid input";
 
     numbers = [];
     ops = [];
-    let operators = ['+', '-', 'x', '/'];
     let tempNum = '';
     for (let i = 0; i < exp.length; i++) {
         
@@ -90,60 +91,21 @@ function evaluate(exp) {
         ops.splice(used_op_idx[idx] - offset, 1);
         offset++;
     }
-
-    // for (let n in numbers) {
-    //     console.log(numbers[n]);
-    // }
-
-    console.log(numbers);
-    console.log(ops);
     
     answer = numbers[0];
     for (let op_idx = 0; op_idx < ops.length; op_idx++) {
         num_idx = op_idx + 1;
         if (ops[op_idx] === '+') {
-            // next_num = Number(op_idx) * 2 + 1;
-            // console.log(Number(numbers[op_idx]));
             answer = addNum(answer, Number(numbers[num_idx]));
         }
         else if ((ops[op_idx] === '-')) {
-            // next_num = Number(op_idx) * 2 + 1;
-            // answer += subtractNum(Number(numbers[Number(op_idx)*2]), Number(numbers[next_num]));
             answer = subtractNum(answer, Number(numbers[num_idx]));
         }
     }
     return answer;
-    // for (let op in ops) {
-    //     // console.log(ops[op]);
-    //     if (!first_num_added) {
-    //         addNumber(answer, numbers[num_idx]);
-    //         num_idx++;
-    //         false_num_added = true;
-    //     }
-    //     if (ops[op] === '+' && first_num_added) {
-    //         addNumber(answer, numbers[++num_idx]);
-    //         num_idx++;
-    //     }
-    //     else if (ops[op] === '-' && first_num_added) {
-    //         subtractNumber(numbers[num_idx], numbers[++num_idx]);
-    //         num_idx++;
-    //     }
-    //     else if (ops[op] === 'x' && first_num_added) {
-    //         multiplyNumber(numbers[num_idx], numbers[++num_idx]);
-    //         num_idx++;
-    //     }
-    //     else if (ops[op] === '%' && first_num_added) {
-    //         divideNumber(numbers[num_idx], numbers[++num_idx]);
-    //         num_idx++;
-    //     }
-    // }
-
-    // for (let n in numbers) {
-    //     console.log(numbers[n]);
-    // }
 }
 
-function displayInput(button) {
+function displayInput_alt_ver(button) {
     let input = button.target.textContent;
     if (input === 'C') {
         display.value = '0';
@@ -152,22 +114,83 @@ function displayInput(button) {
         display.value = display.value.slice(0, -1)
     }
     else if (input === '=') {
-        // else  if (input in ['+', '-', 'x', '%']) {
-        //     numbers.push(tempNumber);
-        //     operators.push()
-        // }
         display.value = evaluate(display.value);
     }
     else {
-        if (display.value === '0' || isNaN(display.value) || display.value === "ERROR") {
+        if (display.value === '0' || display.value === "NaN" || display.value === "ERROR") {
             display.value = button.target.textContent;
         }
         else {
             display.value += button.target.textContent;
         }
-
     }
 }
+
+function displayInput(button) {
+    let input = button.target.textContent;
+    if (input === 'C') {
+        display.value = '0';
+        operator_in_use = '';
+        is_input_saved = false;
+        take_another_num = false;
+        ans = 0;
+    }
+    else if (input === '←') {
+        display.value = display.value.slice(0, -1)
+    }
+    else if (input === '=') {
+        if (is_input_saved && operator_in_use !== '') {
+            ans = operate(ans, display.value, operator_in_use);
+            display.value = ans;
+            take_another_num = true;
+        }
+        // display.value = evaluate(display.value);
+    }
+    else if (operators.includes(input)) {
+        if (!is_input_saved) { 
+            operator_in_use = input;
+            ans = display.value;
+            is_input_saved = true;
+            take_another_num = true;
+        }
+        else {
+            if (!take_another_num) {
+                ans = operate(ans, display.value, operator_in_use);
+                display.value = ans;
+                operator_in_use = input;
+                take_another_num = true;
+            }
+            else {
+                operator_in_use = input;
+            }
+        }
+    }
+    else {
+        if (take_another_num) {
+            display.value = input;
+            console.log("Eval");
+            take_another_num = false;
+        }
+        else if (display.value === '0' || display.value === "NaN" || display.value === "ERROR") {
+            display.value = input;
+            operator_in_use = '';
+            is_input_saved = false;
+            take_another_num = false;
+            ans = 0;
+            console.log("Error");
+        }
+        else {
+            display.value += input;
+            console.log("add");
+        }
+    }
+}
+
+let take_another_num = false;
+let operator_in_use = '';
+let is_input_saved = false;
+let operators = ['+', '-', 'x', '/'];
+let ans = 0;
 
 //list of numbers and operators from left to right
 let numbers = [];
